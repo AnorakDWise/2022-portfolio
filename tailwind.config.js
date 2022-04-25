13dee72b3ba10525dfa9050aeda4376e6f3e1718
AnorakDWise <gregory442005@gmail.com>
2022-04-25 20:03:20 +0200

    rebuilded from ground up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,9 @@
-const stoneBlack = "#1c1917";
-const stoneWhite = "#f5f5f4";
-const black = "#000000";
-
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      borderRadius: {
-        "4xl": "2rem",
-      },
-      colors: {
-        transparentWhite: "rgba(68, 64, 60, 0.50)",
-      },
-
-      backgroundImage: {
-        defaultWallpaper: "url('../assets/images/background.svg')",
-      },
-
       fontFamily: {
-        nunito: "'Nunito', sans-serif",
-      },
-
-      dropShadow: {
-        whiteNeon: "0px 7.5px 10px " + stoneWhite,
-        defaultShadow: "0px 7.5px 15px " + black,
-        defaultFarShadow: "0px 7.5px 30px " + black,
+        rubik: "'Rubik', sans-serif",
       },
     },
   },
